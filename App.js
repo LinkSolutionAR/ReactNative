2a7f8916ce2d4a9beb5a3941c78f76d075fbc61c
Santiago Semhan <santisemhan2@gmail.com>
2020-09-10 13:58:00 -0300

    Creando Base

--- App.js
+++ App.js
@@ -1,79 +1,127 @@
+// React Native Navigation Drawer – Example using Latest Navigation Version //
+// https://aboutreact.com/react-native-navigation-drawer //
+import "react-native-gesture-handler";
+
 import * as React from "react";
-import { Button, View } from "react-native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Button, View, Text, TouchableOpacity, Image } from "react-native";
+
 import { NavigationContainer } from "@react-navigation/native";
-import Ionicons from "react-native-vector-icons/Ionicons";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Ionicons
-        reverse
-        name="ios-home"
-        type="ionicon"
-        color="#517fa4"
-        size={100}
-      />
-      <Button
-        onPress={() => navigation.navigate("Notifications")}
-        title="Go to notifications"
-      />
-    </View>
-  );
-}
+import HomeScreen from "./pages/HomeScreen";
+import MiPerfilScreen from "./pages/MiPerfilScreen";
+import MiPropiedad from "./pages/MiPropiedad";
 
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Ionicons
-        reverse
-        name="ios-american-football"
-        type="ionicon"
-        color="#517fa4"
-        size={30}
-      />
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
-
+const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
-export default function App() {
+const NavigationDrawerStructure = (props) => {
+  //Structure for the navigatin Drawer
+  const toggleDrawer = () => {
+    //Props to open/close the drawer
+    props.navigationProps.toggleDrawer();
+  };
+
+  return (
+    <View style={{ flexDirection: "row" }}>
+      <TouchableOpacity onPress={() => toggleDrawer()}>
+        {/*Donute Button Image */}
+        <Image
+          source={require("./Imagenes/drawerWhite.png")}
+          style={{ width: 25, height: 25, marginLeft: 5 }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+function firstScreenStack({ navigation }) {
+  return (
+    <Stack.Navigator initialRouteName="HomeScreen">
+      <Stack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          title: "First Page", //Set Header Title
+          headerLeft: () => (
+            <NavigationDrawerStructure navigationProps={navigation} />
+          ),
+          headerStyle: {
+            backgroundColor: "#f4511e", //Set Header color
+          },
+          headerTintColor: "#fff", //Set Header text color
+          headerTitleStyle: {
+            fontWeight: "bold", //Set Header text style
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function secondScreenStack({ navigation }) {
+  return (
+    <Stack.Navigator
+      initialRouteName="MiPerfilScreen"
+      screenOptions={{
+        headerLeft: () => (
+          <NavigationDrawerStructure navigationProps={navigation} />
+        ),
+        headerStyle: {
+          backgroundColor: "#f4511e", //Set Header color
+        },
+        headerTintColor: "#fff", //Set Header text color
+        headerTitleStyle: {
+          fontWeight: "bold", //Set Header text style
+        },
+      }}
+    >
+      <Stack.Screen
+        name="MiPerfilScreen"
+        component={MiPerfilScreen}
+        options={{
+          title: "Second Page", //Set Header Title
+        }}
+      />
+      {/* <Stack.Screen
+        name="MiPropiedad"
+        component={MiPropiedad}
+        options={{
+          title: "Third Page", //Set Header Title
+        }}
+      /> */}
+    </Stack.Navigator>
+  );
+}
+
+function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Navigator
+        drawerContentOptions={{
+          activeTintColor: "#e91e63",
+          itemStyle: { marginVertical: 5 },
+        }}
+      >
         <Drawer.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            drawerIcon: (config) => (
-              <Ionicons
-                reverse
-                name="ios-home"
-                type="ionicon"
-                color="#517fa4"
-                size={30}
-              />
-            ),
-          }}
+          name="HomeScreen"
+          options={{ drawerLabel: "First page Option" }}
+          component={firstScreenStack}
         />
         <Drawer.Screen
-          name="Notifications"
-          component={NotificationsScreen}
-          options={{
-            drawerIcon: (config) => (
-              <Ionicons
-                reverse
-                name="ios-create"
-                type="ionicon"
-                color="#517fa4"
-                size={30}
-              />
-            ),
-          }}
+          name="MiPerfilScreen"
+          options={{ drawerLabel: "Second page Option" }}
+          component={secondScreenStack}
+        />
+        <Drawer.Screen
+          name="MiPropiedad"
+          options={{ drawerLabel: "Third Page Option" }}
+          component={MiPropiedad}
         />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
