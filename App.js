2c751704a602c5cebbe9e88335c67941626b3e0e
Santiago <santisemhan2@gmail.com>
2020-09-16 16:26:17 -0300

    Creando segunda Base

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import React from "react";
-import * as eva from "@eva-design/eva";
-import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
-import { EvaIconsPack } from "@ui-kitten/eva-icons";
-import { AppNavigator } from "./component/navigation.component";
+import { View, Text, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import MainTabScreen from "./screens/MainTabScreen";
 
-export default () => (
-  <>
-    <IconRegistry icons={EvaIconsPack} />
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <AppNavigator />
-    </ApplicationProvider>
-  </>
-);
+import { DrawerContent } from "./screens/DrawerContent";
+import SupportScreen from "./screens/SupportScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import BookmarkScreen from "./screens/BookmarkScreen";
+
+const Drawer = createDrawerNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator drawerContent={(props) => <DrawerContent {...props} />}>
+        <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
+        <Drawer.Screen name="SupportScreen" component={SupportScreen} />
+        <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+        <Drawer.Screen name="BookmarkScreen" component={BookmarkScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
